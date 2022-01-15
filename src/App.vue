cbef5b8a5a7dd94e019799ba33e63eb1f5b6bdc7
Webstain <ayman.makhoukhi@gmail.com>
2022-01-16 00:57:20 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,112 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<banner-component
+  mode="square"
+  carousel-on-mobile="false"
+  :items="[
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/500',
+      link: 'https://example.com/link1'
+		},
+    {
+			type: 'cta',
+			title: 'CTA Title',
+      button: 'CTA Button',
+      link: 'https://example.com/link2'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/400',
+      link: 'https://example.com/link3'
+		}
+	]"
+>
+</banner-component>
+
+
+<banner-component
+  mode="square"
+  carousel-on-mobile="true"
+  :items="[
+    {
+			type: 'cta',
+			title: 'CTA Title',
+      button: 'CTA Button',
+      link: 'https://example.com/link1'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/700',
+      link: 'https://example.com/link2'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/800',
+      link: 'https://example.com/link3'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/900',
+      link: 'https://example.com/link3'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/1000',
+      link: 'https://example.com/link3'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/1200',
+      link: 'https://example.com/link3'
+		}
+	]"
+>
+</banner-component>
+
+
+
+<banner-component
+  mode="rectangle"
+  carousel-on-mobile="false"
+  :items="[
+		{
+			type: 'image',
+			aspectRatio: 'rectangle',
+			src: 'https://picsum.photos/1200/400',
+      link: 'https://example.com/link1'
+		},
+		{
+			type: 'image',
+			aspectRatio: 'square',
+			src: 'https://picsum.photos/800',
+      link: 'https://example.com/link1'
+		},
+    {
+			type: 'cta',
+			title: 'CTA Title',
+      button: 'CTA Button',
+      link: 'https://example.com/link1'
+		}
+	]"
+>
+</banner-component>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BannerComponent from './components/banner-component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BannerComponent
   }
 }
 </script>
@@ -19,8 +116,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-decoration: none;
 }
 </style>
---
+++ src/components/banner-component.vue
@@ -0,0 +1,128 @@
+<template>
+        <div :class="[ mode == 'square' ? 'Banner banner-square' : 'Banner banner-rectangular']">
+            <div v-for="item in items" :key="item" class="bannerItem"
+                :class="[ (mode=='rectangle' && item.aspectRatio=='rectangle') ? 'with-img-desktop' :
+                          (mode=='rectangle' && item.aspectRatio=='square') ? 'with-img-mobile' :
+                          (mode=='square' && item.aspectRatio=='square') ? 'default' : '']" >
+
+                <a :href="item.link">
+                    <img v-if="item.type=='image'" 
+                        :src="item.src">
+
+                    <div v-else class="cta">
+                        <span>{{item.title}}</span>
+                        <button>{{item.button}}</button>
+                    </div>
+
+                </a>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    mode: String,
+    items: Array,
+    "carousel-on-mobile" : Boolean
+  },
+}
+</script>
+
+<style scoped>
+a{
+   text-decoration: none;
+}
+.Banner{
+  margin:auto;
+  width:85%;
+  margin-bottom:20px;
+}
+.banner-square{
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+}
+.banner-rectangular{
+  display: grid;
+  grid-template-columns: 2fr 1fr
+}
+.banner-square .bannerItem{
+  aspect-ratio: 1 / 1;  
+}
+.banner-rectangular img,
+.banner-square img{
+  width:100%;
+  height:100%;
+  display: block;
+}
+
+.banner-rectangular .with-img-mobile{
+  display:none;
+}
+.cta{
+  aspect-ratio: 1 / 1;
+  height:100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color:#D95FA7;
+  text-align: center;
+  color:white;
+}
+.cta span{
+  font-size:2em;
+  line-height:1.4;
+  padding: 1rem 2rem;
+}
+.cta button{
+  height: 40px;
+  min-width:200px;
+  border : 2px solid white;
+  background: transparent;
+  color:white;
+  transition: all .5s;
+  min-width: 220px;
+}
+.cta button:hover{
+  background: white;
+  color:black;
+}
+/* Portrait tablets and small desktops */
+@media (min-width: 768px) and (max-width: 991px) {
+    .Banner{
+      width:100%;
+    }
+    .cta span{
+      font-size: 1.5rem;
+    }
+}
+/* Landscape phones and portrait tablets */
+@media (max-width: 767px) {
+  .Banner{
+      width:100%;
+  }
+ .banner-rectangular,.banner-square{
+    grid-template-columns: 1fr;
+  }
+  .cta{
+    aspect-ratio: 2 / 1;
+  }
+  .cta span{
+      font-size: 1.5rem;
+  }
+  .cta button{
+        width: 80%
+  }
+  .banner-square .bannerItem{
+    aspect-ratio: 0/0;  
+  }
+  .banner-rectangular .with-img-mobile{
+    display:block;
+  }
+  .banner-rectangular .with-img-desktop{
+    display:none;
+  }
+}
+</style>
